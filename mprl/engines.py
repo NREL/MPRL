558e0f3a856c31dce5cffa337b75d44eeabdaed9
Nick Wimer <nwimer@nrel.gov>
2019-11-12 14:40:08 -0700

    changed reactor engine Tinj from 900 K to 300 K

--- mprl/engines.py
+++ mprl/engines.py
@@ -621,7 +621,7 @@ class ReactorEngine(Engine):
         *args,
         agent_steps=100,
         dt=5e-6,  # Time step for integrating the 0D reactor (s)
-        Tinj=900.0,  # Injection temperature of fuel/air mixture (K)
+        Tinj=300.0,  # Injection temperature of fuel/air mixture (K)
         minj=0.0002,  # Mass of injected fuel/air mixture (kg)
         max_injections=1,  # Maximum number of injections allowed
         **kwargs,
